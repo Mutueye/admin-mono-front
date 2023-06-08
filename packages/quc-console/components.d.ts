37e74631b099f2fefc1b9b546d336d2603a40a16
Mutueye <mutuyueniao@gmail.com>
2023-06-08 15:09:55 +0800

    refactor: quc-console config eslint & prettier ignore

--- packages/quc-console/components.d.ts
+++ packages/quc-console/components.d.ts
@@ -7,16 +7,16 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton'];
-    ElForm: typeof import('element-plus/es')['ElForm'];
-    ElFormItem: typeof import('element-plus/es')['ElFormItem'];
-    ElInput: typeof import('element-plus/es')['ElInput'];
-    ElMenu: typeof import('element-plus/es')['ElMenu'];
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem'];
-    ElScrollbar: typeof import('element-plus/es')['ElScrollbar'];
-    ElSubMenu: typeof import('element-plus/es')['ElSubMenu'];
-    RouterLink: typeof import('vue-router')['RouterLink'];
-    RouterView: typeof import('vue-router')['RouterView'];
-    ScrollableContent: typeof import('./src/components/ScrollableContent.vue')['default'];
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    ScrollableContent: typeof import('./src/components/ScrollableContent.vue')['default']
   }
 }
